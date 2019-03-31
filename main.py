6d0304667f799079d074e5f88961b47dddb97dcc
Kaito Koike <kaikai.poteti@keio.jp>
2019-03-31 12:50:57 +0900

    fix requirement

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route("/signup",method=["POST"])
+def signup():
+    html = """
+    <form action="/test">
+        <p><label>test: </label>
+        <input type="text" name="test" value="default">
+        <button type="submit" formmethod="post">POST</button></p>
+    </form>
+    """
+    return Markup(html)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
