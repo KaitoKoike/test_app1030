17f3740eaec603ba238f567011ef0d0bd158d911
Kaito Koike <kaikai.poteti@keio.jp>
2019-03-31 13:21:52 +0900

    fix requirement

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask, request, Markup
 
 app = Flask(__name__)
 
+person = {}
+
+
+
 @app.route("/")
 def index():
     return "Hello World"
@@ -11,7 +15,7 @@ def index():
 def register():
     html = """
     <form action="/signup">
-        <p><label>test: </label>
+        <p><label>ID(nick name): </label>
         <input type="text" name="id" value="aaaa"> </br>
         <label>pass: </label>
         <input type="text" name="pass" value="0000">
@@ -21,9 +25,26 @@ def register():
     return Markup(html)
 
 @app.route("/signup",methods=["POST"])
+
 def add_person():
     if request.method == "POST":
-        return request.form["id"]
+        success_msg = {
+                    "message": "Account successfully created",
+                    "user": {
+                    "user_id": "",
+                    "nickname": ""
+                    }
+                    }
+        person_id = request.form["id"]
+        person_pass = request.form["pass"]
+        if person_id not in person:
+            person[person_id] = {"pass":person_pass}
+            success_msg["user_id"] = person_id
+            success_msg["nickname"] = person_id
+            return success_msg,200
+        else:
+
+        
     else:
         return "" ,204
 
