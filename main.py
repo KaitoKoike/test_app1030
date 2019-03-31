846b4af849f678e2d97d5eb2fe5caf6c82f56f14
Kaito Koike <kaikai.poteti@keio.jp>
2019-03-31 13:55:40 +0900

    fix requirement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, Markup
+import json
 
 app = Flask(__name__)
 
@@ -18,8 +19,10 @@ def index():
 def register():
     html = """
     <form action="/signup">
-        <p><label>ID(nick name): </label>
+        <p><label>ID: </label>
         <input type="text" name="id" value="aaaa"> </br>
+        <label>Nick Name: </label>
+        <input type="text" name="nickname" value="AAAa"> </br>
         <label>pass: </label>
         <input type="text" name="pass" value="0000">
         <button type="submit" formmethod="post">Sign UP</button></p>
@@ -40,20 +43,45 @@ def add_person():
                     }
         person_id = request.form["id"]
         person_pass = request.form["pass"]
+        person_nick = request.form["nickname"]
         if person_id not in person:
             person[person_id] = {"pass":person_pass}
-            success_msg["user_id"] = person_id
-            success_msg["nickname"] = person_id
-            return success_msg,200
+            success_msg["user"]["user_id"] = person_id
+            success_msg["user"]["nickname"] = person_nick
+            print(person)
+            return json.dumps(success_msg),200
         else:
             error_msg = {
                 "message": "Account creation failed",
                 "cause": "required user_id and password"
                 }
-            return error_msg,400
+            return json.dumps(error_msg),400
     else:
         return "" ,204
 
+@app.route("/users",methods=["GET"])
+def get_person():
+    person_id, person_pass = request.heaers["Authorization"].split(":")
+    if person_id in person and person[person_id]["password"] == person_pass:
+        success_msg = {
+        "message": "User details by user_id",
+        "user": {
+        "user_id": "TaroYamada",
+        "nickname": "",
+        "comment": ""
+            }
+        }
+        success_msg["user_id"] = person_id
+        if person[person_id]["nickname"]:
+            success_msg["nickname"] = person[person_id]["nickname"]
+        else:
+            success_msg["nickname"] = person[person_id]
+        if person[person_id]["comment"]:
+            success_msg["comment"] = person[person_id]["comment"]
+        return json.dumps(success_msg),200
+    else:
+        error_msg = { "message":"Authentication Faild" }
+        return json.dumps(error_msg) , 401
 
 
 if __name__ == "__main__":
