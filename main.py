8ad7f240d9e784fdc4959ec404d49194eda97c78
Kaito Koike <kaikai.poteti@keio.jp>
2019-03-31 13:03:46 +0900

    fix requirement

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
-@app.route("/signup",methods=["POST"])
+@app.route("/signup")
 def register():
     html = """
     <form action="/test">
         <p><label>test: </label>
-        <input type="text" name="test" value="default">
+        <input type="text" name="test" value="aaaa">
+        <input type="text" name="pass" value="0000"
         <button type="submit" formmethod="post">POST</button></p>
     </form>
     """
     return Markup(html)
+
+@app.rout("/signup",methods=["POST"])
+def add_person():
+    print(request.get_jason())
+    return "",204
+
 
 
 if __name__ == "__main__":
